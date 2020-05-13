339cfb54cc786d33ab6bfe86407f2a7a8c053b0f
vevanonarain <60396096+vevanonarain@users.noreply.github.com>
2020-05-13 21:10:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var hypnoticBall, database;
+var position;
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game=new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  
+}
